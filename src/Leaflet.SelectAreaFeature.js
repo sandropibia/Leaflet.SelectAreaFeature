ad94d671059d8fdac70aa706dcb34991ab1513fd
Sandro Pibia <sandro.pibia@outlook.com>
2018-02-16 23:32:57 +0100

    update js file

--- src/Leaflet.SelectAreaFeature.js
+++ src/Leaflet.SelectAreaFeature.js
@@ -90,11 +90,11 @@
 	_doMouseMove: function(ev) {
 
 	  this._ARR_latlon.push(ev.latlng);
-	  if (this._pre_latlon === '' || this._pre_latlon === "undefined") {
+	  if (this._pre_latlon == '' || this._pre_latlon == "undefined") {
 		this._pre_latlon = ev.latlng;
 		this._ARR_latlon_line.push(this._pre_latlon);
 	  }
-	  else if ( this._pre_latlon !== '' && ( this._post_latlon === '' || this._post_latlon == "undefined") ) {
+	  else if ( this._pre_latlon != '' && ( this._post_latlon == '' || this._post_latlon == "undefined") ) {
 		this._post_latlon = ev.latlng;
 		this._ARR_latlon_line.push(this._post_latlon);
 	  }
@@ -105,8 +105,8 @@
 		this._ARR_latlon_line.push(this._post_latlon);
 	  }
 	  
-	  if ( this._pre_latlon !== '' && this._post_latlon !== '' ) {
-        if ( this._area_line_new === '' && this._area_line === '' ) {
+	  if ( this._pre_latlon != '' && this._post_latlon != '' ) {
+        if ( this._area_line_new == '' && this._area_line == '' ) {
 		  this._area_line = L.polyline(this._ARR_latlon_line, {
 			                                 color: this.options.color, 
 											 weight: this.options.weight, 
@@ -115,7 +115,7 @@
 										   
 		  this._area_line.addTo(this._map);
         }
-        if ( this._area_line_new === '' && this._area_line !== '' ) {
+        if ( this._area_line_new == '' && this._area_line != '' ) {
 		  this._area_line_new = L.polyline(this._ARR_latlon_line, {
 			                                 color: this.options.color, 
 											 weight: this.options.weight, 
@@ -125,7 +125,7 @@
 		  this._area_line_new.addTo(this._map);
 		  this._map.removeLayer(this._area_line);
         }
-        if ( this._area_line_new !== '' && this._area_line !== '' ) {
+        if ( this._area_line_new != '' && this._area_line != '' ) {
 		  this._area_line = L.polyline(this._ARR_latlon_line, {
 			                                 color: this.options.color, 
 											 weight: this.options.weight, 
@@ -168,32 +168,32 @@
 	     pol = this._area_pologon_layers[_i].getBounds();
 	   
 	     this._map.eachLayer(function(layer){
-           if ( (layertype === 'polygon' || layertype === 'all') && layer instanceof L.Polygon && !pol.equals(layer.getBounds()) ) {
+           if ( (layertype == 'polygon' || layertype == 'all') && layer instanceof L.Polygon && !pol.equals(layer.getBounds()) ) {
 		     if ( pol.contains(layer.getBounds()) ) {
               layers_found.push(layer);
 		     }	 
 	       }
-           if ( (layertype === 'polyline' || layertype === 'all') && layer instanceof L.Polyline && !pol.equals(layer.getBounds()) ) {
+           if ( (layertype == 'polyline' || layertype == 'all') && layer instanceof L.Polyline && !pol.equals(layer.getBounds()) ) {
 		     if (  pol.contains(layer.getBounds()) ) {
               layers_found.push(layer);
 		     }  
 		   }   
-           if ( (layertype === 'circle' || layertype === 'all') && layer instanceof L.Circle && !pol.equals(layer.getBounds()) ) {
+           if ( (layertype == 'circle' || layertype == 'all') && layer instanceof L.Circle && !pol.equals(layer.getBounds()) ) {
 		     if ( pol.contains(layer.getBounds()) ) {
               layers_found.push(layer);
 		     }  
 		   }   
-           if ( (layertype === 'rectangle' || layertype === 'all') && layer instanceof L.Rectangle && !pol.equals( layer.getBounds()) ) {
+           if ( (layertype == 'rectangle' || layertype == 'all') && layer instanceof L.Rectangle && !pol.equals( layer.getBounds()) ) {
 	         if ( pol.contains(layer.getBounds()) ) {
               layers_found.push(layer);
 		     }  
 		   }  
-           if ( (layertype === 'marker' || layertype === 'all') && layer instanceof L.Marker  ) {
+           if ( (layertype == 'marker' || layertype == 'all') && layer instanceof L.Marker  ) {
 	         if ( pol.contains(layer.getLatLng()) ) {
               layers_found.push(layer);
 		     }
 		   }  
-           if ( (layertype === 'circlemarker' || layertype === 'all') && layer instanceof L.CircleMarker  ) {
+           if ( (layertype == 'circlemarker' || layertype == 'all') && layer instanceof L.CircleMarker  ) {
 	         if ( pol.contains(layer.getLatLng()) ) {
               layers_found.push(layer);
 		     }
@@ -201,7 +201,7 @@
          });
 	     _i++;
 	   }
-	   if ( layers_found.length === 0  ){
+	   if ( layers_found.length == 0  ){
 		   layers_found = null;
 	   }
 		   
@@ -213,4 +213,3 @@
 }, window));
 
 L.Map.addInitHook('addHandler', 'selectAreaFeature', L.SelectAreaFeature);
-
